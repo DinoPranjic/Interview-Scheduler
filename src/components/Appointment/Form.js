cf2ab70a4ccae56f214ca5259d0642271001f3af
Dino <dinopranjic92@gmail.com>
2022-05-17 18:07:42 -0400

    add props and state to form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form (props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = function () {
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  const cancel = () => {
+    reset();
+    props.onCancel()
+  }
+
   return(
   <main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
-    <form autoComplete="off">
+    <form autoComplete="off" onSubmit={e => e.preventDefault()}>
       <input
         className="appointment__create-input text--semi-bold"
         name="name"
         type="text"
+        value={student}
         placeholder="Enter Student Name"
-        /*
-          This must be a controlled component
-          your code goes here
-        */
+        onChange={(e) => setStudent(e.target.value)}
       />
     </form>
     <InterviewerList 
-      /* your code goes here */
+      interviewers={props.interviewers}
+      value={interviewer}
+      onChange={setInterviewer}
     />
   </section>
   <section className="appointment__card-right">
     <section className="appointment__actions">
-      <Button danger>Cancel</Button>
-      <Button confirm>Save</Button>
+      <Button danger onClick={cancel}>Cancel</Button>
+      <Button confirm onSave={props.onSave}>Save</Button>
     </section>
   </section>
 </main>
